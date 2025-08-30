7661d98af45f4887d34fcc7e58f42219d57dab05
habibaSaleem123 <habiba_saleem16@icloud.com>
2025-08-31 01:14:47 +0500

    Electron prod: HashRouter + CRA homepage + pin Electron; option A packaging

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
 
